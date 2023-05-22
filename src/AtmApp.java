00ed96ae7a198ae56f4bc4925043c27a13a38609
Fatih Akkaya <akkaya064@gmail.com>
2023-05-22 12:35:31 +0100

    V2

--- src/AtmApp.java
+++ src/AtmApp.java
@@ -4,5 +4,10 @@ public class AtmApp {
     public static void main(String[] args) {
         //Version: V1
         System.out.println("This an Atm Project");
+
+        //Version: V2
+        System.out.println("Java is easy");
+
+
     }
 }
