26b07f539f1d35881be5c4b5b636dca5e4fd94df
Fatih Akkaya <akkaya064@gmail.com>
2023-05-22 12:34:28 +0100

    V1

---
+++ src/AtmApp.java
@@ -0,0 +1,8 @@
+import java.sql.SQLOutput;
+
+public class AtmApp {
+    public static void main(String[] args) {
+        //Version: V1
+        System.out.println("This an Atm Project");
+    }
+}
